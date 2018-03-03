e63451938b261104176eff75322e81cd1d0a6974
David Basalla <davidbasalla@gmail.com>
2018-03-03 14:38:13 +0000

    Split out building definitions from main file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OBJLoader } from "three-obj-loader-es6";
 import { TrackballControls } from "three-trackballcontrols";
+import srcModelsDefs from "./buildings.js";
 
 var scene = new THREE.Scene();
 
@@ -26,64 +27,6 @@ document.body.appendChild(renderer.domElement);
 var light = new THREE.PointLight(0xffffff, 10, 100);
 light.position.set(50, 50, 50);
 scene.add(light);
-
-let originalModels = {};
-const srcModelsDefs = [
-  {
-    objFilePath: "../assets/Building_Super_Market.obj",
-    imgFilePath: "../assets/Building_Super_Market.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [0, 0, -3]
-  },
-  {
-    objFilePath: "../assets/Building_Factory.obj",
-    imgFilePath: "../assets/Building_Factory.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [-10, 0, 0]
-  },
-  {
-    objFilePath: "../assets/Building_Gas_Station.obj",
-    imgFilePath: "../assets/Building_Gas_Station.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [0, 0, 10]
-  },
-  {
-    objFilePath: "../assets/Building_Sky_big_color01.obj",
-    imgFilePath: "../assets/Building_Sky_big_color01.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [-12, 0, -11]
-  },
-  {
-    objFilePath: "../assets/Building_Sky_big_color01.obj",
-    imgFilePath: "../assets/Building_Sky_big_color02.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [12, 0, -11]
-  },
-  {
-    objFilePath: "../assets/Building_Sky_small_color01.obj",
-    imgFilePath: "../assets/Building_Sky_small_color01.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [8, 0, -11]
-  },
-  {
-    objFilePath: "../assets/Building_Pizza.obj",
-    imgFilePath: "../assets/Building_Pizza.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [13, 0, -3]
-  },
-  {
-    objFilePath: "../assets/Building_Fast_Food.obj",
-    imgFilePath: "../assets/Building_Fast_Food.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [-12, 0, 10]
-  },
-  {
-    objFilePath: "../assets/Building_Restaurant.obj",
-    imgFilePath: "../assets/Building_Restaurant.png",
-    objScale: [0.01, 0.01, 0.01],
-    position: [10, 2.1, 8]
-  }
-];
 
 var size = 50;
 var divisions = 50;
