1e57997a33b9070ec5a2126249506e774014c9d4
David Basalla <davidbasalla@gmail.com>
2018-03-03 20:09:36 +0000

    Reverse scaling and use const

--- src/app.js
+++ src/app.js
@@ -3,55 +3,55 @@ import { OBJLoader } from "three-obj-loader-es6";
 import { TrackballControls } from "three-trackballcontrols";
 import srcModelsDefs from "./buildings.js";
 
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 // CAMERA
-var aspect = window.innerWidth / window.innerHeight;
-var d = 2000;
-var offsetX = -3000;
-var offsetY = -1000;
-var camera = new THREE.OrthographicCamera(
+const aspect = window.innerWidth / window.innerHeight;
+const d = 20;
+const offsetX = -18;
+const offsetY = -5;
+const camera = new THREE.OrthographicCamera(
   -d * aspect + offsetX,
   d * aspect + offsetX,
   d + offsetY,
   -d + offsetY,
-  0.01,
-  10000
+  1,
+  1000
 );
 
-camera.position.set(5000, 3500, 5000); // all components equal
-camera.lookAt(0, 0, 0); // or the origin
+camera.position.set(40, 30, 40); // all components equal
+camera.lookAt(scene.position);
 
 // RENDERER
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x888888);
 document.body.appendChild(renderer.domElement);
 
 // LIGHTS
-var light = new THREE.HemisphereLight(0xddddff, 0x556655, 1.75);
+const light = new THREE.HemisphereLight(0xddddff, 0x556655, 1.75);
 scene.add(light);
 
-var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(directionalLight);
 
-var targetObject = new THREE.Object3D();
+const targetObject = new THREE.Object3D();
 scene.add(targetObject);
 targetObject.position.set(-5, 0, 0);
 directionalLight.target = targetObject;
 
 // PLANE
-var geometry = new THREE.PlaneGeometry(100000, 100000, 32);
-var material = new THREE.MeshBasicMaterial({
+const geometry = new THREE.PlaneGeometry(1000, 1000, 32);
+const material = new THREE.MeshBasicMaterial({
   color: 0x44bb44,
   side: THREE.DoubleSide
 });
-var plane = new THREE.Mesh(geometry, material);
+const plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
 plane.rotation.x += 1.5708;
 
 // LOADING MODELS
-var loadOriginalModels = () => {
+const loadOriginalModels = () => {
   const loader = new OBJLoader();
 
   srcModelsDefs.map(modelDef => {
@@ -61,6 +61,7 @@ var loadOriginalModels = () => {
     loader.load(
       modelDef.objFilePath,
       function(object) {
+        object.scale.set(...modelDef.scale);
         object.traverse(child => {
           if (child instanceof THREE.Mesh) {
             child.material = material;
@@ -68,7 +69,7 @@ var loadOriginalModels = () => {
         });
 
         // scene.add(object);
-        var newObject = object.clone();
+        const newObject = object.clone();
         newObject.position.set(...modelDef.position);
         scene.add(newObject);
       },
@@ -86,7 +87,7 @@ loadOriginalModels();
 
 camera.position.z = 10;
 
-var animate = function() {
+const animate = function() {
   requestAnimationFrame(animate);
 
   // cube.rotation.x += 0.1;
--- src/buildings.js
+++ src/buildings.js
@@ -2,46 +2,61 @@ export default [
   {
     objFilePath: "../assets/Building_Super_Market.obj",
     imgFilePath: "../assets/Building_Super_Market.png",
-    position: [0, 0, -300]
+    scale: [0.01, 0.01, 0.01],
+    position: [0, 0, -3]
   },
   {
     objFilePath: "../assets/Building_Factory.obj",
     imgFilePath: "../assets/Building_Factory.png",
-    position: [-1000, 0, 0]
+    scale: [0.01, 0.01, 0.01],
+    position: [-10, 0, 0]
   },
   {
     objFilePath: "../assets/Building_Gas_Station.obj",
     imgFilePath: "../assets/Building_Gas_Station.png",
-    position: [0, 0, 1000]
+    scale: [0.01, 0.01, 0.01],
+    position: [0, 0, 10]
   },
   {
     objFilePath: "../assets/Building_Sky_big_color01.obj",
     imgFilePath: "../assets/Building_Sky_big_color01.png",
-    position: [-1200, 0, -1100]
+    scale: [0.01, 0.01, 0.01],
+    position: [-12, 0, -11]
   },
   {
     objFilePath: "../assets/Building_Sky_big_color01.obj",
     imgFilePath: "../assets/Building_Sky_big_color02.png",
-    position: [1200, 0, -1100]
+    scale: [0.01, 0.01, 0.01],
+    position: [12, 0, -11]
   },
   {
     objFilePath: "../assets/Building_Sky_small_color01.obj",
     imgFilePath: "../assets/Building_Sky_small_color01.png",
-    position: [800, 0, -1100]
+    scale: [0.01, 0.01, 0.01],
+    position: [8, 0, -11]
   },
   {
     objFilePath: "../assets/Building_Pizza.obj",
     imgFilePath: "../assets/Building_Pizza.png",
-    position: [1300, 0, -300]
+    scale: [0.01, 0.01, 0.01],
+    position: [13, 0, -3]
   },
   {
     objFilePath: "../assets/Building_Fast_Food.obj",
     imgFilePath: "../assets/Building_Fast_Food.png",
-    position: [-1200, 0, 1000]
+    scale: [0.01, 0.01, 0.01],
+    position: [-12, 0, 10]
   },
   {
     objFilePath: "../assets/Building_Restaurant.obj",
     imgFilePath: "../assets/Building_Restaurant.png",
-    position: [1000, 200, 800]
+    scale: [0.01, 0.01, 0.01],
+    position: [10, 2, 8]
+  },
+  {
+    objFilePath: "../assets/Building_Restaurant.obj",
+    imgFilePath: "../assets/Building_Restaurant.png",
+    scale: [0.01, 0.01, 0.01],
+    position: [10, 2, 8]
   }
 ];
