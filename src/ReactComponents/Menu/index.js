39f11ff1a95af62fb2d509bc08fca57696071d5a
David Basalla <davidbasalla@gmail.com>
2018-03-17 16:06:07 +0000

    Simplify React component

--- src/ReactComponents/Menu/index.js
+++ src/ReactComponents/Menu/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 import styles from "./style.css";
 
-class Menu extends React.Component {
-  render() {
-    const { turn, selectedTile } = this.props.state;
-    const { nextTurn } = this.props;
+const Menu = props => {
+  const { turn, selectedTile } = props.state;
 
-    return (
-      <div className="menu">
-        <h2>Day {turn}</h2>
-        <div>
-          <h3>{selectedTile}</h3>
-        </div>
-
-        <button className="endTurnButton" onClick={nextTurn}>
-          End turn
-        </button>
+  return (
+    <div className="menu">
+      <h2>Day {turn}</h2>
+      <div>
+        <h3>{selectedTile}</h3>
       </div>
-    );
-  }
-}
+
+      <button className="endTurnButton" onClick={props.nextTurn}>
+        End turn
+      </button>
+    </div>
+  );
+};
 
 export default Menu;
